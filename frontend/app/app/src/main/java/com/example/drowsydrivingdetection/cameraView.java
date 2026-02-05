18a472f21e8d0cd5c77508a38e827002bd5c90c1
Tony Shou <a.mashou04@gmail.com>
2026-02-05 04:19:12 -0500

    Add OpenCV integration and update .gitignore
    - Integrated OpenCV by adding OpenCV-release.aar to dependencies in build.gradle.
    - Updated .gitignore to remove build directories since they constantly update

--- frontend/app/app/src/main/java/com/example/drowsydrivingdetection/cameraView.java
+++ frontend/app/app/src/main/java/com/example/drowsydrivingdetection/cameraView.java
@@ -2,10 +2,14 @@ package com.example.drowsydrivingdetection;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.opencv.android.OpenCVLoader;
 
 public class cameraView extends AppCompatActivity {
 
@@ -17,6 +21,14 @@ public class cameraView extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_page);
+
+        if (OpenCVLoader.initLocal()) {
+            Log.d("LOADED", "OpenCV successfully loaded.");
+        } else {
+            Log.e("LOADED", "OpenCV initialization failed!");
+            (Toast.makeText(this, "OpenCV initialization failed!", Toast.LENGTH_LONG)).show();
+            return;
+        }
 
         returnToScreen();
     }
