124e632c565a8e0e8c441e63e779bf5d649bd568
Tony Shou <a.mashou04@gmail.com>
2026-02-06 00:32:11 -0500

    Add JavaCameraView to camera page and implement camera view listener in cameraView class

--- frontend/app/app/src/main/java/com/example/drowsydrivingdetection/cameraView.java
+++ frontend/app/app/src/main/java/com/example/drowsydrivingdetection/cameraView.java
@@ -3,18 +3,23 @@ package com.example.drowsydrivingdetection;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
-public class cameraView extends AppCompatActivity {
+public class cameraView extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     // Anthony
     Button returnButton;
+    JavaCameraView javaCameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -29,6 +34,10 @@ public class cameraView extends AppCompatActivity {
             (Toast.makeText(this, "OpenCV initialization failed!", Toast.LENGTH_LONG)).show();
             return;
         }
+
+        javaCameraView = (JavaCameraView) findViewById(R.id.my_camera);
+        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        javaCameraView.setCvCameraViewListener(cameraView.this);
 
         returnToScreen();
     }
@@ -45,6 +54,21 @@ public class cameraView extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onCameraViewStarted(int i, int i1) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame cvCameraViewFrame) {
+        return null;
     }
 
 }
