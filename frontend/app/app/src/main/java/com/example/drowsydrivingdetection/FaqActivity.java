8921c036775996984e10df17e936d5975c630103
Nirav Patel <127055509+nrpatel370@users.noreply.github.com>
2026-02-10 22:51:17 -0500

    added Anthony's xml code for the FAQ page

--- frontend/app/app/src/main/java/com/example/drowsydrivingdetection/FaqActivity.java
+++ frontend/app/app/src/main/java/com/example/drowsydrivingdetection/FaqActivity.java
@@ -7,7 +7,7 @@ public class FaqActivity extends NavActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.open_cv_activity); //placeholder
+        setContentView(R.layout.faq_page);
 
         setupBottomNavigation();
     }
