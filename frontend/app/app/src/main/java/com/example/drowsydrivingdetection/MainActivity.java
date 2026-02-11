0eaf6c3d044a85f58c39e792f064363b728bfd65
Nirav Patel <127055509+nrpatel370@users.noreply.github.com>
2026-02-11 12:06:27 -0500

    added shared preferences to main activity to direct user to login/register page if not logged in

--- frontend/app/app/src/main/java/com/example/drowsydrivingdetection/MainActivity.java
+++ frontend/app/app/src/main/java/com/example/drowsydrivingdetection/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.drowsydrivingdetection;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -29,6 +31,17 @@ public class MainActivity extends NavActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences sharedPreferences = getSharedPreferences("DrowsyDriverPrefs", MODE_PRIVATE);
+        boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);
+
+        if (!isLoggedIn) {
+            Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
         setContentView(R.layout.home_page);
 
         alertCountNumber = findViewById(R.id.alertCountNumber);
