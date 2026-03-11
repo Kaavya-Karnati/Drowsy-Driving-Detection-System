2ee3d7a0cfe426a71a6863ddfe4ee625beb1ff2b
Tony Shou <a.mashou04@gmail.com>
2026-03-11 01:41:36 -0400

    Added Ahmed's chart commits

--- app/app/src/main/java/com/example/drowsydrivingdetection/AlertsDashboard.java
+++ app/app/src/main/java/com/example/drowsydrivingdetection/AlertsDashboard.java
@@ -4,6 +4,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import android.graphics.Color;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.util.ArrayList;
+
 import com.google.android.material.imageview.ShapeableImageView;
 
 public class AlertsDashboard extends NavActivity{
@@ -11,6 +21,8 @@ public class AlertsDashboard extends NavActivity{
     private TextView audioAlertDetectionText;
     private TextView visualAlertDetectionText;
     private ShapeableImageView profileIcon;
+
+    private BarChart Bchart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +49,9 @@ public class AlertsDashboard extends NavActivity{
 
         setupBottomNavigation();
         loadProfilePicture();
+
+        Bchart = findViewById(R.id.Barchart);
+        chartman();
     }
 
     private int getCurrentAudioAlertCounts(){
@@ -67,6 +82,39 @@ public class AlertsDashboard extends NavActivity{
     private String getUserPhoto() {
         String email = sharedPreferences.getString("userEmail", "guest");
         return "profilePhotoUri_" + email;
+    }
+
+    private void chartman() {
+
+
+        int aAlerts = 5;
+        int vAlerts = 1;
+
+        ArrayList<BarEntry> inputs = new ArrayList<>();
+        inputs.add(new BarEntry(0, aAlerts));
+        inputs.add(new BarEntry(1, vAlerts));
+
+        BarDataSet dataset = new BarDataSet(inputs, "Weekly Alerts");
+
+        dataset.setColors(
+                Color.parseColor("#FBBC05"),
+                Color.parseColor("#FE2C55")
+        );
+
+        dataset.setValueTextSize(14f);
+
+        BarData Bdata = new BarData(dataset);
+        Bdata.setBarWidth(0.6f);
+
+        Bchart.setData(Bdata);
+        String[] labels = {"Auditory", "Visual"};
+        XAxis xAxis = Bchart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+        xAxis.setGranularity(1f);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        Bchart.getAxisRight().setEnabled(false);
+        Bchart.getDescription().setText("Audio vs Visual Alerts This Week");
+        Bchart.invalidate();
     }
 
 }
