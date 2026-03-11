492930d58eeb77ce45d5c272bfd476e35a40c99b
Kaavya <kaavya.karnati@yottaasys.com>
2026-03-11 02:13:41 -0400

    Had few errors with the past loading data of the alerts and function. They are now removed.

--- app/app/src/main/java/com/example/drowsydrivingdetection/MainActivity.java
+++ app/app/src/main/java/com/example/drowsydrivingdetection/MainActivity.java
@@ -3,30 +3,8 @@ package com.example.drowsydrivingdetection;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends NavActivity {
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
-
-//    public void openCamera(View view) {
-//        startActivity(new Intent(this, ModelPage.class));
-//    }
-//
-//    public void loadOpenCV(View view) {
-//        startActivity(new Intent(this, OpenCV.class));
-//    }
-//
-//    public void openProfile(View view) {
-//        startActivity(new Intent(this, ProfilePage.class));
-//    }
-    private TextView alertCountNumber;
-    private TextView auditoryAlertEmail;
-    private TextView visualAlertEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,20 +21,6 @@ public class MainActivity extends NavActivity {
         }
 
         setContentView(R.layout.home_page);
-/*
-        alertCountNumber = findViewById(R.id.alertCountNumber);
-        auditoryAlertEmail = findViewById(R.id.auditoryAlertEmail);
-        visualAlertEmail = findViewById(R.id.visualAlertEmail);
-*/
         setupBottomNavigation();
-        loadHomeData();
     }
-
-    private void loadHomeData() {
-        alertCountNumber.setText("45,678 Alerts");
-        auditoryAlertEmail.setText("+20 % from last month");
-        visualAlertEmail.setText("-5% from last month");
-    }
-
-
 }
